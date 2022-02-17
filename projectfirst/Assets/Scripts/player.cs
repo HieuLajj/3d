40c1a8f70ae39391ba3144726f2a37ccb3c854cc
tomatowet <63393556+tomatowet@users.noreply.github.com>
2022-02-17 20:02:52 +0700

    buoi2.1

--- projectfirst/Assets/Scripts/player.cs
+++ projectfirst/Assets/Scripts/player.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce=  5f;
+    int wholeNumber =3;
+    float decimalNumber =3.45f;
+    string text ="blabla";
+    bool condition = false;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("toi la lai van hieu");
+        rb =   GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space")){
-            Debug.Log("dan an space");
-           GetComponent<Rigidbody>().velocity = new Vector3(0,5,0);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontalInput*movementSpeed, rb.velocity.y ,verticalInput*movementSpeed);
+        if (Input.GetButtonDown("Jump")){
+              rb.velocity = new Vector3(rb.velocity.x,jumpForce,rb.velocity.z);
         }
-        if (Input.GetKey("w")){
-            GetComponent<Rigidbody>().velocity = new Vector3(0,0,5);
-        }
-        if (Input.GetKey("d")){
-            GetComponent<Rigidbody>().velocity = new Vector3(5,0,0);
-        }
-        
+         
     }
 }
